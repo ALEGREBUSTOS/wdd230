a2be9dd8c195f1348cbec45c8205a5910ef555bd
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-02-20 18:20:49 -0700

    chamber index home added

---
+++ chamber/scripts/hamburger-botton.js
@@ -0,0 +1,7 @@
+const hbgButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav-bar');
+
+hbgButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hbgButton.classList.toggle('open');
+});
