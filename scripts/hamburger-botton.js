b7de5ead6f80d828f1d7cffcd47118a4ef5319d2
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-02-27 16:04:31 -0700

    button temple name fixed

--- scripts/hamburger-botton.js
+++ scripts/hamburger-botton.js
@@ -1,7 +1,14 @@
 const hbgButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const templename = document.querySelector('.temple-name');
 
 hbgButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hbgButton.classList.toggle('open');
-});
+    if (navigation.classList.contains('open')) {
+        templename.style.position = 'absolute';
+        templename.style.top = '710px';
+    } else {
+        templename.style.top = '460px';
+    }
+})
