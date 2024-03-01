464b400d05a35187e806fda94665c01f58457a24
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-02-29 19:26:23 -0700

    lazy load activity added

---
+++ lesson07/scripts/getdates.js
@@ -0,0 +1,27 @@
+function lastmodification() {
+
+    const doc = document;
+
+    doc.lastModified
+
+    return doc.lastModified
+
+}
+
+function currentyear(){
+    let today=new Date();
+    
+    let currentyr=today.getFullYear()
+
+    return currentyr
+}
+
+function updatefooter(lastmodification,currentyear){
+
+    const doc=document
+    doc.querySelector('#lastModified').textContent=`Last modification ${lastmodification}`
+    doc.querySelector('#current-year').textContent=`© ${currentyear} Alejo Alegre Bustos  - Argentina `
+}
+
+
+updatefooter(lastmodification(),currentyear())
