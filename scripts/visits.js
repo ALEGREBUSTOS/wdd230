a830eab1cd06766823fa0660215540518d681efa
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-03-02 16:19:11 -0700

    number of visits fixed

--- scripts/visits.js
+++ scripts/visits.js
@@ -3,6 +3,11 @@ let number_visits = Number(window.localStorage.getItem("visits-ls"))
 
 if (number_visits == 0) {
     visits.textContent = "Welcome! this is your first visit!";
+
 } else {
+    
     visits.textContent = `Number of Visits: ${number_visits}`
 }
+
+number_visits++
+localStorage.setItem("visits-ls",number_visits)
