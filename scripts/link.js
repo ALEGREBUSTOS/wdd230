516a561abf75dde93904b96e7eeb1ef41aa03b66
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-03-11 14:38:56 -0600

    adding activities to dashboard activities

--- scripts/link.js
+++ scripts/link.js
@@ -1,9 +1,9 @@
-const giturl = "https://github.com/ALEGREBUSTOS/wdd230/data/links.json";
+const giturl = "https://github.com/ALEGREBUSTOS/wdd230/blob/08bc003fcc0dbb667ff8bdb249a138e34120f681/data/links.json";
 
 async function getlinks() {
-    const response = await fetch(linksURL);
+    const response = await fetch(giturl);
     const data = await response.json();
     console.log(data);
   }
   
-  getlinks();
+  getlinks(giturl);
