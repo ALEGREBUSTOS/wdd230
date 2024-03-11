bec54559ffd2fc8285a1e4e4510fca2aa7f52f17
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-03-11 15:24:13 -0600

    adding properties to json file

--- scripts/link.js
+++ scripts/link.js
@@ -1,9 +1,13 @@
-const giturl = "https://github.com/ALEGREBUSTOS/wdd230/blob/08bc003fcc0dbb667ff8bdb249a138e34120f681/data/links.json";
+const giturl = "https://alegrebustos.github.io/wdd230/data/links.json";
 
 async function getlinks() {
     const response = await fetch(giturl);
     const data = await response.json();
-    console.log(data);
+    displaydata(data);
   }
   
   getlinks(giturl);
+
+function displaydata(){
+    
+}
