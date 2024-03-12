b1b20a6eac7e2bce9d9ad68bc3dace93948794fe
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-03-12 11:33:17 -0600

    adding memberships

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -12,7 +12,10 @@ const h4Element2 = document.querySelector('.advertisement-2 h4');
 const aElement2 = document.querySelector('.advertisement-2 a');
 const h4Element3 = document.querySelector('.advertisement-3 h4');
 const aElement3 = document.querySelector('.advertisement-3 a');
-const navbarelemnts= document.querySelectorAll('.nav-bar a')
+const navbarelemnts = document.querySelectorAll('.nav-bar a')
+const htmlnamee = document.URL.substring(document.URL.lastIndexOf("/") + 1)
+
+
 
 
 modeButton.addEventListener("click", () => {
@@ -21,6 +24,7 @@ modeButton.addEventListener("click", () => {
         body.style.color = "#FEFAE0";
         header.style.background = "#283618";
         header.style.color = "#FEFAE0";
+        if (htmlnamee == "index.html") {
         eventweatherbox.style.background = "#283618";
         eventweatherbox.style.color = "#FEFAE0";
         eventbox.style.background = "#283618";
@@ -35,23 +39,55 @@ modeButton.addEventListener("click", () => {
         advertisementboxes[1].style.color = "#FEFAE0";
         advertisementboxes[2].style.background = "#283618";
         advertisementboxes[2].style.color = "#FEFAE0";
-        navbarelemnts.forEach(function(a) {
-            a.style.color = "#FEFAE0"; 
-            a.style.background = "#283618";
-        });
-
         h4Element1.style.color = "#FEFAE0";
         aElement1.style.color = "#FEFAE0";
         h4Element2.style.color = "#FEFAE0";
         aElement2.style.color = "#FEFAE0";
         h4Element3.style.color = "#FEFAE0";
         aElement3.style.color = "#FEFAE0";
+        }
+        if (htmlnamee == "join.html") {
+            const mform = document.querySelector(".wf1");
+            const legend = document.querySelector(".wf1 legend");
+            mform.style.background = "#283618";
+            mform.style.color = "#FEFAE0";
+            legend.style.background = "#283618";
+            legend.style.color = "#FEFAE0";
+        }
+
+        if (htmlnamee == "thanks.html") {
+            const tmain = document.querySelector(".thkmain");
+            const h3 = document.querySelector(".thkmain h3");
+            const p = document.querySelector(".thkmain p");
+            tmain.style.background = "#283618";
+            tmain.style.color = "#FEFAE0";
+            h3.style.background = "#283618";
+            h3.style.color = "#FEFAE0";
+            p.style.background = "#283618";
+            p.style.color = "#FEFAE0";
+        }
+
+        if (htmlnamee == "discover.html") {
+            const main = document.querySelector("main")
+            const h3titles = document.querySelectorAll("h3")
+            main.style.background = "#283618";
+            main.style.color = "#FEFAE0";
+            h3titles.forEach(function (h3) {
+                h3.style.borderBottomColor = "#FEFAE0";
+            });
+        }
+
+        navbarelemnts.forEach(function (a) {
+            a.style.color = "#FEFAE0";
+            a.style.background = "#283618";
+        });
         modeButton.textContent = "☀️";
     } else {
         body.style.background = "#FEFAE0";
         body.style.color = "#283618";
         header.style.background = "#FEFAE0";
         header.style.color = "#283618";
+        if (htmlnamee == "index.html") {
         eventweatherbox.style.background = "#FEFAE0";
         eventweatherbox.style.color = "#283618";
         eventbox.style.background = "#FEFAE0";
@@ -66,16 +102,46 @@ modeButton.addEventListener("click", () => {
         advertisementboxes[1].style.color = "#283618";
         advertisementboxes[2].style.background = "#FEFAE0";
         advertisementboxes[2].style.color = "#283618";
-        navbarelemnts.forEach(function(a) {
-            a.style.color = "#283618"; 
-            a.style.background = "#FEFAE0";
-        });
         h4Element1.style.color = "#283618";
         aElement1.style.color = "#283618";
         h4Element2.style.color = "#283618";
         aElement2.style.color = "#283618";
         h4Element3.style.color = "#283618";
         aElement3.style.color = "#283618";
+        }
+        if (htmlnamee == "join.html") {
+            const mform = document.querySelector(".wf1");
+            const legend = document.querySelector(".wf1 legend");
+            mform.style.background = "#FEFAE0";
+            mform.style.color = "#283618";
+            legend.style.background = "#FEFAE0";
+            legend.style.color = "#283618";
+        }
+        if (htmlnamee == "thanks.html") {
+            const tmain = document.querySelector(".thkmain");
+            const h3 = document.querySelector(".thkmain h3");
+            const p = document.querySelector(".thkmain p");
+            tmain.style.background = "#FEFAE0";
+            tmain.style.color = "#283618";
+            h3.style.background = "#FEFAE0";
+            h3.style.color = "#283618";
+            p.style.background = "#FEFAE0";
+            p.style.color = "#283618";
+        }
+        if (htmlnamee == "discover.html") {
+            const main = document.querySelector("main")
+            const h3titles = document.querySelectorAll("h3")
+            main.style.background = "#FEFAE0";
+            main.style.color = "#283618";
+            h3titles.forEach(function (h3) {
+                h3.style.borderBottomColor = "#283618";
+            })
+        }
+        navbarelemnts.forEach(function (a) {
+            a.style.color = "#283618";
+            a.style.background = "#FEFAE0";
+        });
+   
         modeButton.textContent = "🌑";
     }
 });
