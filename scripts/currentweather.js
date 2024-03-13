c03291264f15bc6379ab5ebea18556e635f8f751
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-03-13 17:08:43 -0600

    adding new features to weather js

--- scripts/currentweather.js
+++ scripts/currentweather.js
@@ -5,21 +5,27 @@ const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
+        console.log(data);
+        const city = document.querySelector('#city');
+        const degrees = document.querySelector('#degrees');
+        const description = document.querySelector('#description');
+        const icon = document.querySelector('#icon');
 
-        const city = document.getElementById('city');
-        const degrees = document.getElementById('degrees');
-        const description = document.getElementById('description');
         city.textContent = data.name;
-        let degree_celsius=(data.main.temp - 273.15).toFixed(0)
+        let degree_celsius = (data.main.temp - 273.15).toFixed(0);
         degrees.textContent = `${degree_celsius} ℃`;
-        description.textContent = data.weather[0].description;
-        if (degree_celsius >= 30 ){
-            degrees.style.color='orange'
-        }else{
+        const iconimg = document.createElement('img');
+        iconimg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        iconimg.style.width = "50px";
+        iconimg.style.height = "50px";
+        icon.innerHTML = '';
+        icon.appendChild(iconimg);
+        description.textContent = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1); // Capitalize first letter of description
 
-            degrees.style.color='lightgreen'
-
-            
+        if (degree_celsius >= 30) {
+            degrees.style.color = 'orange';
+        } else {
+            degrees.style.color = 'lightgreen';
         }
     })
     .catch(error => console.error('Error fetching weather data:', error));
