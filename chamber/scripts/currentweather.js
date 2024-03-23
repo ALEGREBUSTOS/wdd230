cb64ecd1526f8603c99dba280ecf3f71b8416aa7
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-03-22 21:28:57 -0600

    adding details weather box

--- chamber/scripts/currentweather.js
+++ chamber/scripts/currentweather.js
@@ -1,45 +1,60 @@
-const apiKey = 'e1e42164e352ebd9cd4ef6db70fe58bd';
+const apikey = 'e1e42164e352ebd9cd4ef6db70fe58bd';
 const lat = '31.6333';
 const lon = '-60.7000';
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const apiurl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}`;
 
-function wtrforecast(url,citySelector, degreesSelectormin,degreesSelectormax, descriptionSelector, iconSelector, index) {
+function background(description, elemclass) {
+    const background = document.querySelector(elemclass);
+    if (background) {
+        const descriptioncontent = description.toLowerCase();
+        if (descriptioncontent.includes('cloud')) {
+            background.style.backgroundImage = "url('images/dayrain-smaill.webp')";
+        }
+        if (descriptioncontent.includes('sunny')) {
+            background.style.backgroundImage = "url('images/sunnyday-small.webp')";
+        }
+        background.style.backdropFilter = "blur(100px)";
+    }
+}
+// Ahora queda hacer que las letras se intenamos hace un blur pero no esta funcionando,
+// Deberiamos agregar un desenfoque por background de parrafo sino
+function wtrforecast(url, cityselector, degreesselectormin, degreesselectormax, descriptionselector, iconselector, index, elementclass) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // console.log(data);
-            const ncity = 'Santa Fe' 
-            const cityElement = document.querySelector(citySelector);
-            const degreesElementmax = document.querySelector(degreesSelectormax);
-            const degreesElementmin = document.querySelector(degreesSelectormin);
-            const descriptionElement = document.querySelector(descriptionSelector);
-            const iconElement = document.querySelector(iconSelector);
+            const ncity = 'Santa Fe';
+            const cityelement = document.querySelector(cityselector);
+            const degreeselementmax = document.querySelector(degreesselectormax);
+            const degreeselementmin = document.querySelector(degreesselectormin);
+            const descriptionelement = document.querySelector(descriptionselector);
+            const iconelement = document.querySelector(iconselector);
 
-            cityElement.textContent = ncity;
+            cityelement.textContent = ncity;
             let degree_celsiusmin = (data.list[index].main.temp_min - 273.15).toFixed(0);
             let degree_celsiusmax = (data.list[index].main.temp_max - 273.15).toFixed(0);
-            degreesElementmin.textContent = `${degree_celsiusmin} ℃`;
-            degreesElementmax.textContent = `${degree_celsiusmax} ℃`;
+            degreeselementmin.textContent = `${degree_celsiusmin} ℃`;
+            degreeselementmax.textContent = `${degree_celsiusmax} ℃`;
             const iconimg = document.createElement('img');
             iconimg.src = `https://openweathermap.org/img/wn/${data.list[index].weather[0].icon}@2x.png`;
             iconimg.style.width = "50px";
             iconimg.style.height = "50px";
-            iconElement.innerHTML = '';
-            iconElement.appendChild(iconimg);
-            descriptionElement.textContent = data.list[index].weather[0].description.charAt(0).toUpperCase() + data.list[index].weather[0].description.slice(1); 
+            iconelement.innerHTML = '';
+            iconelement.appendChild(iconimg);
+            const weatherdescription = data.list[index].weather[0].description.charAt(0).toUpperCase() + data.list[index].weather[0].description.slice(1);
+            descriptionelement.textContent = weatherdescription;
+            background(weatherdescription, elementclass);
 
             if (degree_celsiusmin >= 30 && degree_celsiusmax >= 30) {
-                degreesElementmin.style.color = 'orange';
-                degreesElementmax.style.color = 'orange';
+                degreeselementmin.style.color = 'orange';
+                degreeselementmax.style.color = 'orange';
             } else {
-                degreesElementmin.style.color = 'lightgreen';
-                degreesElementmax.style.color = 'lightgreen';
+                degreeselementmin.style.color = 'lightgreen';
+                degreeselementmax.style.color = 'lightgreen';
             }
-            
         })
         .catch(error => console.error('Error fetching weather data:', error));
 }
 
-wtrforecast(apiUrl,"#city1","#degreesmin1", "#degreesmax1","#description1","#icon1", 5);
-wtrforecast(apiUrl,"#city2","#degreesmin2", "#degreesmax2","#description2","#icon2", 9);
-wtrforecast(apiUrl,"#city3","#degreesmin3", "#degreesmax3","#description3","#icon3", 14);
+wtrforecast(apiurl, "#city1", "#degreesmin1", "#degreesmax1", "#description1", "#icon1", 5, ".ctn-weather");
+wtrforecast(apiurl, "#city2", "#degreesmin2", "#degreesmax2", "#description2", "#icon2", 9, ".ctn-weather2");
+wtrforecast(apiurl, "#city3", "#degreesmin3", "#degreesmax3", "#description3", "#icon3", 14, ".ctn-weather3");
