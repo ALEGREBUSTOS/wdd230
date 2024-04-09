8c8f153424c093456fa63503a915fa66a6284a1f
ALEGREBUSTOS <alejo.alegre@outlook.es>
2024-04-09 15:30:17 -0600

    improving join page

--- chamber/scripts/hamburger-botton.js
+++ chamber/scripts/hamburger-botton.js
@@ -36,48 +36,48 @@ if (htmlname == "join.html") {
 
 
 
-    function showbenefits() {
-        const bnpmem = document.querySelector("#bnpmem");
-        const bmem = document.querySelector("#bmem");
-        const smem = document.querySelector("#smem");
-        const gmem = document.querySelector("#gmem");
+    // function showbenefits() {
+    //     const bnpmem = document.querySelector("#bnpmem");
+    //     const bmem = document.querySelector("#bmem");
+    //     const smem = document.querySelector("#smem");
+    //     const gmem = document.querySelector("#gmem");
 
-        bnpmem.style.display = "none";
-        bmem.style.display = "none";
-        smem.style.display = "none";
-        gmem.style.display = "none";
+    //     bnpmem.style.display = "none";
+    //     bmem.style.display = "none";
+    //     smem.style.display = "none";
+    //     gmem.style.display = "none";
 
-        const npmembership = document.getElementById('np-membership').checked;
-        const bmembership = document.getElementById('b-pmembership').checked;
-        const smembership = document.getElementById('s-membership').checked;
-        const gmembership = document.getElementById('g-membership').checked;
+    //     const npmembership = document.getElementById('np-membership').checked;
+    //     const bmembership = document.getElementById('b-pmembership').checked;
+    //     const smembership = document.getElementById('s-membership').checked;
+    //     const gmembership = document.getElementById('g-membership').checked;
 
-        if (npmembership) {
-            bmem.style.display = "none";
-            smem.style.display = "none";
-            gmem.style.display = "none";
-            bnpmem.style.display = "grid";
-            bnpmem.style.gridRow = "8";
-        } else if (bmembership) {
-            bnpmem.style.display = "none";
-            smem.style.display = "none";
-            gmem.style.display = "none";
-            bmem.style.display = "grid";
-            bmem.style.gridRow = "9";
-        } else if (smembership) {
-            gmem.style.display = "none";
-            bnpmem.style.display = "none";
-            bmem.style.display = "none";
-            smem.style.display = "grid";
-            smem.style.gridRow = "10";
-        } else if (gmembership) {
-            bnpmem.style.display = "none";
-            bmem.style.display = "none";
-            smem.style.display = "none";
-            gmem.style.display = "grid";
-            gmem.style.gridRow = "11";
-        }
-    }
+    //     if (npmembership) {
+    //         bmem.style.display = "none";
+    //         smem.style.display = "none";
+    //         gmem.style.display = "none";
+    //         bnpmem.style.display = "grid";
+    //         bnpmem.style.gridRow = "8";
+    //     } else if (bmembership) {
+    //         bnpmem.style.display = "none";
+    //         smem.style.display = "none";
+    //         gmem.style.display = "none";
+    //         bmem.style.display = "grid";
+    //         bmem.style.gridRow = "9";
+    //     } else if (smembership) {
+    //         gmem.style.display = "none";
+    //         bnpmem.style.display = "none";
+    //         bmem.style.display = "none";
+    //         smem.style.display = "grid";
+    //         smem.style.gridRow = "10";
+    //     } else if (gmembership) {
+    //         bnpmem.style.display = "none";
+    //         bmem.style.display = "none";
+    //         smem.style.display = "none";
+    //         gmem.style.display = "grid";
+    //         gmem.style.gridRow = "11";
+    //     }
+    // }
 
 
 
@@ -97,22 +97,22 @@ if (htmlname == "join.html") {
 
 
 
-    npmembership.addEventListener("change", function () {
+    // npmembership.addEventListener("change", function () {
 
-        showbenefits()
-    });
-    bmembership.addEventListener("change", function () {
+    //     showbenefits()
+    // });
+    // bmembership.addEventListener("change", function () {
 
-        showbenefits()
-    });
-    smembership.addEventListener("change", function () {
+    //     showbenefits()
+    // });
+    // smembership.addEventListener("change", function () {
 
-        showbenefits()
-    });
-    gmembership.addEventListener("change", function () {
+    //     showbenefits()
+    // });
+    // gmembership.addEventListener("change", function () {
 
-        showbenefits()
-    });
+    //     showbenefits()
+    // });
 
 
 
@@ -126,7 +126,7 @@ if (htmlname == "join.html") {
 
         
         event.preventDefault();
-        window.location.href = 'thanks.html';
+        window.location.href = 'thanksyou.html';
     });
 
 
